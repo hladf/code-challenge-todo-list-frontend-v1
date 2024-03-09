56adfccd80748de0b1332c9a63a9260bddb91295
Hugo Ladeira de Freitas <x493098@gruposantander.com>
2024-03-09 11:35:19 -0300

    chore: testando husky no pre-commit

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,8 +4,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hello Code Challenge!</h1>
-      {   process.env
-      .REACT_APP_BASE_URL}
+      {process.env.REACT_APP_BASE_URL}
     </div>
   );
 }
